ede789b1d226a76082e90df0efb23c44fc755228
Kleber Vales <klebervales.dev@gmail.com>
2026-02-24 11:57:52 -0300

    feat: start project with first commit

---
+++ src/main/java/com/finance/financespringmodulith/FinanceSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package com.finance.financespringmodulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSpringModulithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceSpringModulithApplication.class, args);
+    }
+
+}
